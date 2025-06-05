a0d677a3def690c86c986efe6ded1c341e768c50
maxklema <maxklema@gmail.com>
2025-06-05 13:49:35 -0400

    fixes

--- api/server.py
+++ api/server.py
@@ -1,6 +1,6 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.proxies import WebshareProxyConfig
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv
 import os
 from flask_cors import CORS
@@ -8,8 +8,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/url/<video_id>', methods=['GET'])
-def home(video_id):
+@app.route('/', methods=['GET'])
+def home():
+    video_id=request.args.get('video_id')
     ytt_api = YouTubeTranscriptApi(
     proxy_config=WebshareProxyConfig(
         proxy_username=os.getenv("USERNAME"),
